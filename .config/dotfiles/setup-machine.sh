e5a3c8f3d1246081e542cff07d4e75e2d8c632a7
Felix Nehrke <felix@nehrke.info>
2022-03-08 10:54:14 +0100

    Export and source nvm directory and bash-completion

--- .config/dotfiles/setup-machine.sh
+++ .config/dotfiles/setup-machine.sh
@@ -55,5 +55,8 @@ sdk install gradle 7.4
 # install nvm to manage nodejs
 curl -s https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | /bin/bash
 nvm install node
+export NVM_DIR="$XDG_CONFIG_HOME/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 echo "ready for work :-)"
