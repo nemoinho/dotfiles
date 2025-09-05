7a3efc7ac38f72fab5551a0400984cb65c72ab9c
Felix Nehrke <felix@nehrke.info>
2025-09-05 14:07:20 +0200

    Improve fuzzy search in nvim-telescope by using fzf

--- .config/nvim/lua/plugins/nvim-telescope.lua
+++ .config/nvim/lua/plugins/nvim-telescope.lua
@@ -4,18 +4,28 @@ return {
 		'nvim-lua/plenary.nvim',
 		-- do not forget to install ripgrep!!!
 		-- https://github.com/nvim-telescope/telescope.nvim/issues/522#issuecomment-1374795374
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+		},
 	},
 	opts = {
+		extensions = {
+			fzf = {},
+		},
 	},
 	config = function()
-		local ts = require("telescope.builtin")
-		vim.keymap.set("n", "<C-p>", ts.find_files)
-		vim.keymap.set("n", "<Leader>fg", ts.live_grep)
-		vim.keymap.set("n", "<Leader>ff", ts.oldfiles)
-		vim.keymap.set("n", "<Leader>*", ts.grep_string)
-		vim.keymap.set("n", "<Leader>#", ts.grep_string)
-		vim.keymap.set("n", "<Leader>lf", ts.git_bcommits)
-		vim.keymap.set("n", "<Leader>ld", function () ts.git_commits({ file_path = true, use_git_root = false }) end)
-		vim.keymap.set("n", "<Leader>lb", ts.git_branches)
+		local ts = require("telescope")
+		ts.load_extension("fzf")
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<C-p>", builtin.find_files)
+		vim.keymap.set("n", "<Leader>b", "<CMD>Telescope buffers<CR>")
+		vim.keymap.set("n", "<Leader>fg", builtin.live_grep)
+		vim.keymap.set("n", "<Leader>ff", builtin.oldfiles)
+		vim.keymap.set("n", "<Leader>*", builtin.grep_string)
+		vim.keymap.set("n", "<Leader>#", builtin.grep_string)
+		vim.keymap.set("n", "<Leader>lf", builtin.git_bcommits)
+		vim.keymap.set("n", "<Leader>ld", function () builtin.git_commits({ file_path = true, use_git_root = false }) end)
+		vim.keymap.set("n", "<Leader>lb", builtin.git_branches)
 	end,
 }
